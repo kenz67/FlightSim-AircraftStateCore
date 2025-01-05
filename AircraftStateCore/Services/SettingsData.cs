a6d2841bfbee33c49899602dbb7d3f927be6ed24
Kenneth Zaldo <ken@zaldo.com>
2025-01-05 12:13:41 -0500

    Add UT for SettingsData Service

--- AircraftStateCore/Services/SettingsData.cs
+++ AircraftStateCore/Services/SettingsData.cs
@@ -7,47 +7,46 @@ namespace AircraftStateCore.Services;
 
 public class SettingsData(ISettingsRepo settingsRepo) : ISettingsData
 {
-    public Settings Settings { get; set; }
-    private readonly ISettingsRepo _settingsRepo = settingsRepo;
+	public Settings Settings { get; set; }
+	private readonly ISettingsRepo _settingsRepo = settingsRepo;
 
-    public event Func<Task> OnChangeAsync;
+	public event Func<Task> OnChangeAsync;
 
-    public async Task<Settings> ReadSettings()
-    {
-        Settings = await _settingsRepo.GetSettings();
+	public async Task<Settings> ReadSettings()
+	{
+		Settings = await _settingsRepo.GetSettings();
 
-        if (Settings.SelectedData == null)
-        {
-            Settings.SelectedData = new AvailableData().Items;
-        }
-        else   //fill in possible new values
-        {
-            var all = new AvailableData();
-            var missing = all.Items.Where(i => !Settings.SelectedData.Any(i2 => i2.txt == i.txt)).ToList();
-            Settings.SelectedData.AddRange(missing);
-        }
+		if (Settings.SelectedData == null)
+		{
+			Settings.SelectedData = new AvailableData().Items;
+		}
+		else   //fill in possible new values
+		{
+			var all = new AvailableData();
+			var missing = all.Items.Where(i => !Settings.SelectedData.Any(i2 => i2.txt == i.txt)).ToList();
+			Settings.SelectedData.AddRange(missing);
+		}
 
-        return Settings;
-    }
+		return Settings;
+	}
 
-    public async Task UpdatePage()
-    {
-        //TODO - Is this needed?
-        await OnChangeAsync();
-    }
+	public async Task UpdatePage()
+	{
+		await OnChangeAsync();
+	}
 
-    public List<AvailableDataItem> GetSelectedData()
-    {
-        return Settings.SelectedData.Where(s => s.enabled).ToList();
-    }
+	public List<AvailableDataItem> GetSelectedData()
+	{
+		return Settings.SelectedData.Where(s => s.enabled).ToList();
+	}
 
-    public async Task SaveSettings(List<SelectedItem> SelectedItems)
-    {
-        foreach (var item in Settings.SelectedData)
-        {
-            item.enabled = SelectedItems.Exists(s => s.Value.Equals(item.value));
-        }
+	public async Task SaveSettings(List<SelectedItem> SelectedItems)
+	{
+		foreach (var item in Settings.SelectedData)
+		{
+			item.enabled = SelectedItems.Exists(s => s.Value.Equals(item.value));
+		}
 
-        await _settingsRepo.SaveSettings(Settings);
-    }
+		await _settingsRepo.SaveSettings(Settings);
+	}
 }
