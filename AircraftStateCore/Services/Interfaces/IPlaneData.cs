14e6badf5d4837c674400a6e1c9a6ab6675009a9
Kenneth Zaldo <ken@zaldo.com>
2025-10-07 17:25:22 -0400

    Migrate top 1/2 of Apply to Mud, other tweaks

--- AircraftStateCore/Services/Interfaces/IPlaneData.cs
+++ AircraftStateCore/Services/Interfaces/IPlaneData.cs
@@ -4,9 +4,10 @@ namespace AircraftStateCore.Services.Interfaces;
 
 public interface IPlaneData : IPageUpdate
 {
-    PlaneDataStruct CurrentData { get; set; }
-    List<string> Profiles { get; set; }
-    Task LookUpProfile(string profile);
-    Task DeleteProfile(string profile);
-    Task SaveProfile(string profile);
+	PlaneDataStruct CurrentData { get; set; }
+	List<string> Profiles { get; set; }
+	Task LookUpProfile(string profile);
+	Task DeleteProfile(string profile);
+	Task SaveProfile(string profile);
+	Task ResetProfiles();
 }
